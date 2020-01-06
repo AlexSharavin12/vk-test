212ca4cce9e90ad1d24940001df2c379dbd3552f
AlexSharavin12 <59560516+AlexSharavin12@users.noreply.github.com>
2020-01-06 16:46:26 +0500

    Add files via upload

---
+++ precache-manifest.2def4eb96ee0617b0368d2a90605b21e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46543d8d288101093924a0e81e430d1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2c6b3a6fb21d83660af",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "5f3aef2541c5b75661ce",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "a2c6b3a6fb21d83660af",
+    "url": "/static/js/2.4a78dda5.chunk.js"
+  },
+  {
+    "revision": "5f3aef2541c5b75661ce",
+    "url": "/static/js/main.a538df0e.chunk.js"
+  },
+  {
+    "revision": "22946454e66ce463ec45",
+    "url": "/static/js/runtime-main.5b157f45.js"
+  }
+]);
